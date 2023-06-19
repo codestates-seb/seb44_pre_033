8761d0304ff9ac99510ec9b2afa8649d6d6175bd
정지은 <kindjjee@gmail.com>
2023-06-19 16:06:50 +0900

    FE Chore: 질문작성 페이지 마무리

--- client/src/components/ask/DiscardAlarm.jsx
+++ client/src/components/ask/DiscardAlarm.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import { styled } from 'styled-components';
 
 const ModalContainer = styled.div`
   position: fixed;
@@ -9,9 +9,54 @@ const ModalContainer = styled.div`
 `;
 
 const ModalView = styled.div`
-  width: 20vw;
-  height: 15vh;
+  width: 35vw;
+  height: 24vh;
   background-color: #ffffff;
+  border-radius: 0.3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+
+  .titleContainer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .content {
+    color: #3c4145;
+  }
+
+  .title {
+    color: #ae2d31;
+    font-size: 1.8rem;
+    margin-right: 17rem;
+    font-weight: 500;
+  }
+
+  .buttonContainer {
+    display: flex;
+    flex-direction: row;
+    width: 35vw;
+    margin-left: 4vw;
+  }
+
+  .discardButtonText {
+    background-color: #ae2d31;
+    color: white;
+    width: 8rem;
+  }
+  .cancleButtonText {
+    background-color: #ffffff;
+    color: #838a92;
+    margin-left: 30px;
+
+    &:hover {
+      background-color: #f8f9f9;
+    }
+  }
 `;
 
 const ModalBackground = styled.div`
@@ -23,18 +68,50 @@ const ModalBackground = styled.div`
   justify-content: center;
 `;
 
-const DiscardAlarm = () => {
+const DiscardButton = styled.button`
+  width: ${(props) => props.text.length * 10}px;
+  border-radius: 3px;
+  font-size: 1rem;
+  height: 2.5rem;
+`;
+
+const discardButtonText = 'Discard question';
+const cancleButtonText = 'Cancel';
+
+const DiscardAlarm = ({
+  onDiscardQuestion,
+  onCancelButton,
+  handleCloseModal,
+}) => {
   return (
     <ModalContainer>
-      <ModalBackground>
-        <ModalView>
-          <div>Discard question</div>
-          <div>
+      <ModalBackground onClick={handleCloseModal}>
+        <ModalView onClick={(event) => event.stopPropagation()}>
+          <div className="titleContainer">
+            <div className="title">Discard question</div>
+            <button onClick={handleCloseModal}>✕</button>
+          </div>
+          <div className="content">
             Are you sure you want to discard this question? This cannot be
             undone.
           </div>
-          <button>Discard question</button>
-          <button>Cancel</button>
+
+          <div className="buttonContainer">
+            <DiscardButton
+              onClick={onDiscardQuestion} //버튼 클릭시 초기화 및 스크롤 이동처리
+              className="discardButtonText"
+              text={discardButtonText}
+            >
+              {discardButtonText}
+            </DiscardButton>
+            <DiscardButton
+              className="cancleButtonText"
+              text={cancleButtonText}
+              onClick={onCancelButton}
+            >
+              {cancleButtonText}
+            </DiscardButton>
+          </div>
         </ModalView>
       </ModalBackground>
     </ModalContainer>

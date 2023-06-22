5369c18d198d8b4d3546a20a58498b72772c9935
정지은 <kindjjee@gmail.com>
2023-06-22 20:25:56 +0900

    FE: Chore 싸인업 페이지 추가

--- client/src/page/Signup.jsx
+++ client/src/page/Signup.jsx
@@ -1,5 +1,49 @@
+import SnsSignup from '../components/login/SnsSignup.jsx';
+import { styled } from 'styled-components';
+import SignupInput from '../components/login/SignupInput.jsx';
+import { Link } from 'react-router-dom';
+import LeftContent from '../components/login/LeftContent.jsx';
+
+const SignupPageContainer = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #f2f2f4;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  .account {
+    margin-top: 3vh;
+  }
+  .Login {
+    color: #3f77ce;
+  }
+`;
+const SnsSignupContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-right: -5vw;
+`;
+
 const Signup = () => {
-  return <div></div>;
+  return (
+    <SignupPageContainer>
+      <LeftContent />
+      <SnsSignupContainer>
+        <SnsSignup />
+        <SignupInput />
+        <div className="account">
+          Already have an account?{' '}
+          <Link to="/users/login" className="Login">
+            Log in
+          </Link>
+        </div>
+      </SnsSignupContainer>
+    </SignupPageContainer>
+  );
 };
 
 export default Signup;

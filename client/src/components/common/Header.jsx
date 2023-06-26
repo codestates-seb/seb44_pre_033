fb5854a7a6c899d3addf26e951b49717fc3be86f
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-06-26 15:55:14 +0900

    [FE] Chore: 코드정리

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,67 +1,52 @@
 import { styled } from 'styled-components';
 import LogoImg from '../../assets/logo-stackoverflow.svg';
-import { Link } from 'react-router-dom';
-import ButtonFlex from './ButtonFlexible.jsx';
-import 설리 from '../../assets/설리.png';
+import { Link } from 'react-router-dom'
+import ButtonFlex from './ButtonFlexible';
 
-const Header = ({ onLogin }) => {
-  return (
-    <HeaderContainer>
-      <Logo path="/" src={LogoImg} alt="stack overflow" />
-      <ButtonContainer>
-        {onLogin ? (
-          <>
-            <LinkBtn>
-              <UserInfo>
-                <img src={설리} alt="icon"></img>
-                <div>유저이름</div>
-              </UserInfo>
-            </LinkBtn>
-            <LinkBtn>
-              <Link to="/users/logout">
-                <ButtonFlex label="Log out" length="0" color="Blue" />
-              </Link>
-            </LinkBtn>
-          </>
-        ) : (
-          <>
+
+const Header = ()=>{
+    return(
+        <HeaderContainer>
+            <Logo path='/' src={LogoImg} alt='stack overflow'/>
+            <ButtonContainer>
             <Link to="/users/login">
-              <LinkBtn>
-                <ButtonFlex label="Log in" length="0" color="gray" />
-              </LinkBtn>
+                <LinkBtn>
+                    <ButtonFlex label='Log in' length='0' color='gray'/>
+                </LinkBtn>
             </Link>
-            <Link to="/users/signup">
-              <LinkBtn>
-                <ButtonFlex label="Sign up" length="0" color="Blue" />
-              </LinkBtn>
+            <Link to ="/users/signup">
+                <LinkBtn>
+                    <ButtonFlex label='Sign up' length='0' color='Blue'/>
+                </LinkBtn>
             </Link>
-          </>
-        )}
-      </ButtonContainer>
-    </HeaderContainer>
-  );
+            </ButtonContainer>
+        </HeaderContainer>
+    );
 };
 
 export default Header;
 
-const Logo = ({ path, src, alt }) => {
-  return (
-    <LogoWrap>
-      <Link to={path}>
-        <LogoImage src={src} alt={alt} />
-      </Link>
-    </LogoWrap>
-  );
-};
 
+const Logo = ({ path, src, alt }) => {
+    return (
+        <LogoWrap>
+          <Link to={path}>
+            <LogoImage src={src} alt={alt} />
+          </Link>
+        </LogoWrap>
+      );
+};
+    
 const LogoWrap = styled.div`
-  margin: 0.2rem;
-  margin-left: 8vw; //추가
+    margin:0.2rem;
+    margin-left: 8vw; //추가
 `;
 
 const LogoImage = styled.img`
-  width: 10rem;
+    width: 10rem;
 `;
+
+
 const HeaderContainer = styled.div`
     display:flex;
     flex-direction: row;
@@ -77,26 +62,11 @@ const HeaderContainer = styled.div`
     top:0;
 `;
 const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-right: 8vw; //추가
+    display: flex;
+    flex-direction: row;
+    margin-right: 8vw; //추가
   align-items: center; //추가
 `;
 const LinkBtn = styled.div`
-  margin: 0.4rem 0.5rem;
-`;
-
-const UserInfo = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 7.5vw;
-  height: 5vh;
-
-  img {
-    width: 2.5vw;
-    height: 2.5vw;
-    margin-right: 0.5vw;
-  }
+    margin: 0.4rem 0.5rem;
 `;
